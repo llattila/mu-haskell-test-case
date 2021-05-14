5d06529aa35376ef2071a2865927b1e29f12d5b8
lattila <lauri.lattila@simanalytics.com>
2021-05-14 16:45:46 +0300

    Proto-parsings added

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,11 @@ main = do
    areEquals (OneOfFour 1 (B False))
    areEquals (OneOfFour 1 (C 1))
    areEquals (OneOfFour 1 (D 1.0))
+   print "Different parsings"
+   print $ toByteString (OneOfFour 1 (A (T.pack "Some string")))
+   print $ toByteString (OneOfFour 1 (B False))
+   print $ toByteString (OneOfFour 1 (C 1))
+   print $ toByteString (OneOfFour 1 (D 1.0))
 
 areEquals :: OneOfFour -> IO ()
 areEquals oneOfFour =
